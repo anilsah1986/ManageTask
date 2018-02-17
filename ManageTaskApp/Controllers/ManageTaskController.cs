dd182f1023a7e62a3c65555db11f5e88abea3266
anilsah1986 <36566776+anilsah1986@users.noreply.github.com>
2018-02-17 22:08:30 +0530

    Upload ManageTaskApp Projects

---
+++ ManageTaskApp/Controllers/ManageTaskController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ManageTaskApp.DataAccess.Data;
+using ManageTaskApp.DataAccess.DBEntities;
+using ManageTaskApp.Services.APIResponse;
+using ManageTaskApp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ManageTaskApp.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/task/[Action]")]
+    public class ManageTaskController : Controller
+    {
+        private readonly IManageTask _IManageTask;
+        private APIResponse response;
+        private readonly JsonSerializerSettings _serializerSettings;
+
+        public ManageTaskController(IManageTask IManageTask)
+        {
+            _IManageTask = IManageTask;
+            response = new APIResponse();
+            _serializerSettings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore
+            };
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                response = _IManageTask.GetAllTask();
+                var json = JsonConvert.SerializeObject(response, _serializerSettings);
+                return new OkObjectResult(json);
+            }
+            catch { throw; }
+
+        }
+
+        [HttpGet]
+        public IActionResult GetById(long id)
+        {
+            try
+            {
+                response = _IManageTask.GetTaskById(id);
+                var json = JsonConvert.SerializeObject(response, _serializerSettings);
+                return new OkObjectResult(json);
+            }
+            catch { throw; }
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ManageTask model)
+        {
+            if (model == null)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                response = _IManageTask.AddTask(model);
+                var json = JsonConvert.SerializeObject(response, _serializerSettings);
+                return new OkObjectResult(json);
+            }
+            catch { throw; }
+        }
+
+        [HttpPost]
+        public IActionResult Update([FromBody] ManageTask model)
+        {
+            if (model == null)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                response = _IManageTask.UpdateTask(model);
+                var json = JsonConvert.SerializeObject(response, _serializerSettings);
+                return new OkObjectResult(json);
+            }
+            catch { throw; }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(long id)
+        {
+            try
+            {
+                response = _IManageTask.DeleteTask(id);
+                var json = JsonConvert.SerializeObject(response, _serializerSettings);
+                return new OkObjectResult(json);
+            }
+            catch { throw; }
+        }
+    }
+}
+
---
+++ ManageTaskApp/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ManageTaskApp.DataAccess.Data;
+using ManageTaskApp.Services.Interfaces;
+using ManageTaskApp.Services.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ManageTaskApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddTransient<IManageTask, ManageTaskService>();
+            services.AddDbContext<ManageTaskContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
