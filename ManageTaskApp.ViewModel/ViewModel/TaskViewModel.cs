43ca4dc5bb0790db01dbaa8e44ebc7c2d090d39a
anilsah1986 <36566776+anilsah1986@users.noreply.github.com>
2018-02-17 22:27:18 +0530

    Upload ManageTask.ViewModel Projects

---
+++ ManageTaskApp.ViewModel/ViewModel/TaskViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManageTaskApp.ViewModel.ViewModel
+{
+    public class TaskViewModel
+    {
+        public Int64 Id { get; set; }
+        public string Name { get; set; }
+        public string Priority { get; set; }
+        public DateTime Date { get; set; }
+        public string EstimatedCost { get; set; }
+        public string Description { get; set; }
+    }
+}
